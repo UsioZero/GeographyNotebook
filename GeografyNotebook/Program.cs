0097db0450c24858eb80bd5418917d5afbfca3cc
usioZero <swortaartonlineforever@gmail.com>
2022-06-23 20:49:42 +0200

    2.3 info

--- GeografyNotebook/Program.cs
+++ GeografyNotebook/Program.cs
@@ -9,7 +9,7 @@ namespace GeografyNotebook
     internal static class Program
     {
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
